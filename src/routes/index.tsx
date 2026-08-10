9e2f6b513b49653fcbb4881d67d4306c39be8070
Debdatta Panda <pandadebdatta9@gmail.com>
2026-08-10 13:26:01 +0530

    fix: re-add ContributorsCarousel import in index.tsx to fix runtime ReferenceError

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -33,6 +33,7 @@ import { Reveal, SectionHeader, Counter } from "@/components/site/Primitives";
 import { FAQSection } from "@/components/site/FAQ";
 import { CanvasBackground } from "@/components/site/CanvasBackground";
 import { WhatWeDoGallery } from "@/components/site/WhatWeDoGallery";
+import { ContributorsCarousel } from "@/components/site/ContributorsCarousel";
 import {
   STATS,
   MARQUEE,
